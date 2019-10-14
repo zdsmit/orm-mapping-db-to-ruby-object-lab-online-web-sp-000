01f1c8703bc0d86df38b58e7c65796c27897be9b
Zach  Smith <zdsmith001@gmail.com>
2019-10-14 20:32:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,7 +15,7 @@ class Student
     sql = <<-SQL
     SELECT * FROM students
     SQL
-    DB[:conn].execute(sql).each do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
   end
