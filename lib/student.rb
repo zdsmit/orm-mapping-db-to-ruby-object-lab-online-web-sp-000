437f5296f4a8786d3f905a7ecaadf046a123ce02
Zach  Smith <zdsmith001@gmail.com>
2019-10-14 20:40:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -36,7 +36,7 @@ class Student
     end
   end
 
-  def self.all_students_below_12th_grade
+  def self.students_below_12th_grade
     sql = <<-SQL
     SELECT * FROM students WHERE grade < 12
     SQL
