76a43b70c3e9ca22000d0ee068f9879c417d7e48
Zach  Smith <zdsmith001@gmail.com>
2019-10-14 20:19:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,16 +22,16 @@ class Student
     # find the student in the database given a name
     # return a new instance of the Student class
   end
-  
+
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
-  
+
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
